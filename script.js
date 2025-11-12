7230ce6cdce7c7f6ee552bfa5d9359d504a5f58c
Qandor <109076431+HackerZsalmale@users.noreply.github.com>
2025-11-12 18:51:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+let robotnyeres = "0";
+let tenyeres = "0";
+let korok = "0";
+let szazalek = "0";
 let choosen = "";
+let wins = 0; 
+let robotWins = 0;
+let totalGames = 0;
+
 
 document.getElementById("rock").onclick = () => {
   choosen = "rock";
@@ -14,6 +23,7 @@ document.getElementById("scissors").onclick = () => {
 };
 
 function Submit() {
+  totalGames ++;
   const options = ["rock", "paper", "scissors"];
   robot = options[Math.floor(Math.random() * 3)];
 
@@ -25,8 +35,10 @@ function Submit() {
     (choosen === "scissors" && robot === "paper")
   ) {
     document.getElementById("eredmeny").innerText = "Eredmény: Nyertél :D";
+    wins++;
   } else {
     document.getElementById("eredmeny").innerText = "Eredmény: Veszítettél :c";
+    robotnyeres ++;
   }
 
   document.getElementById("retry").style.visibility = "visible";
@@ -52,6 +64,7 @@ function Submit() {
 
   document.getElementById("te").innerText = `Te Választásod: ${choosen}`;
   document.getElementById("robot").innerText = `Robot Választása: ${robot}`;
+  toltes();
 }
 
 document.getElementById("retry").onclick = () => {
@@ -61,4 +74,30 @@ document.getElementById("retry").onclick = () => {
   document.getElementById("eredmeny").innerText = "Eredmény: ";
   document.getElementById("te").innerText = `Te Választásod: ${choosen}`;
   document.getElementById("robot").innerText = `Robot Választása: ${robot}`;
+  szazalek = "0";
+  fill = "0";
+  wins = "0";
+  robotWins = "0";
+  totalGames = "0";
+  const bar = document.getElementById('progress');
+  bar.style.width = szazalek + '%';
+  bar.innerText = Math.round(szazalek) + '%';
 };
+
+function toltes() {
+  const bar = document.getElementById('progress');
+
+  if (totalGames === 0) {
+    bar.style.display = 'none';
+    return;
+  } else {
+    bar.style.display = 'block';
+  }
+
+  let szazalek = (wins / totalGames) * 100;
+  szazalek = Math.min(Math.max(szazalek, 0), 100);
+
+  bar.style.width = szazalek + '%';
+  bar.innerText = Math.round(szazalek) + '%';
+}
+
